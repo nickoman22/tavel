28ba00e685998a92e50eab601269e4248d5a06c1
nickoman22 <nickoman2222@gmail.com>
2025-02-14 20:14:33 +0200

    Update script.js

--- script.js
+++ script.js
@@ -78,7 +78,7 @@ function updateProgress(index, isChecked) {
 function updateProgressBar() {
   const progress = (checkedItems.length / destinations.length) * 100;
   progressFill.style.width = `${progress}%`;
-  progressText.textContent = `${Math.round(progress)}% Δεν εχει δώρο αν δεν το κάνουμε όντως${progress === 100 ? '💖💖' : '🎉'}`;
+  progressText.textContent = `${Math.round(progress)}% Δεν εχει δώρο αν δεν το κάνουμε όντως ${progress === 100 ? '💖💖' : '🎉'}`;
 }
 
 // Event listeners
